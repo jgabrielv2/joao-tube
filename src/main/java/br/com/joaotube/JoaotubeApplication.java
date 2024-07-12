f51b33e1a309f400e2f47c3829575a3f83e170f5
jgabrielv2 <jgabrielv2@outlook.com>
2024-07-12 15:42:43 -0300

    commit inicial

---
+++ src/main/java/br/com/joaotube/JoaotubeApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaotube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaotubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoaotubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joaotube/JoaotubeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaotube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaotubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
