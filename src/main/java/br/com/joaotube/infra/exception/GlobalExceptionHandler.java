2579840218e82582bee08adec3091849c5d4b184
jgabrielv2 <joao.vidal@aluno.unb.br>
2024-07-13 09:31:16 -0300

    refatorado GlobalExceptionHandler

--- src/main/java/br/com/joaotube/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/joaotube/infra/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.joaotube.infra.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,20 +15,23 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(VideoNotFoundException.class)
-    public ResponseEntity<?> tratarErroVideoNaoEncontrado(VideoNotFoundException ex) {
+    public ResponseEntity<String> tratarErroVideoNaoEncontrado(VideoNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> tratarViolacaoRestricaoCampo(ConstraintViolationException ex) {
+        List<DadosErroValidacao> erros = ex.getConstraintViolations().stream().map(violation -> new DadosErroValidacao(
+                violation.getPropertyPath().toString(),
+                violation.getMessage())).toList();
+        return ResponseEntity.badRequest().body(erros);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> tratarErro400(MethodArgumentNotValidException ex) {
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
         List<FieldError> erros = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
-    private record DadosErroValidacao(String campo, String mensagem) {
-        public DadosErroValidacao(FieldError error) {
-            this(error.getField(), error.getDefaultMessage());
-        }
-    }
 
 }
